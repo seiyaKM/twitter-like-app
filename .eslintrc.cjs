449a56e89e132d87b95fa8a24ba8dff939b4bcee
seiyaKM <seiya.kumada.0107@gmail.com>
2023-03-20 23:31:57 +0900

    add linter setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,28 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
+    '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/block-lang': [
+      'error',
+      {
+        script: {
+          lang: 'ts'
+        }
+      }
+    ]
   }
-}
+};
