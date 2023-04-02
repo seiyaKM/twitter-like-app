87adf4cee512232a80c8bfa7400716d16bb443bc
seiyaKM <seiya.kumada.0107@gmail.com>
2023-04-03 06:15:24 +0900

    add vuetify

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <h1 class="login">Login</h1>
-  <p class="login__message">Hello</p>
+  <div>
+    <h1 class="login">Login</h1>
+    <p class="login__message">Hello</p>
+    <v-btn>button</v-btn>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .login {
+  width: 600px;
   color: red;
   &__message {
     color: blue;
